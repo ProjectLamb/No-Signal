6e37b2c71ba12919f340b995bf293be92070dcdb
HobakDG <cil1216@naver.com>
2025-03-20 17:54:46 +0900

    [feat][doc] 내비게이션에 사용할 미니맵 생성

--- Assets/Scripts/MiniCam.cs
+++ Assets/Scripts/MiniCam.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerCam : MonoBehaviour
+public class MiniCam : MonoBehaviour
 {
 
     private Vector2 lastMousePos;
-
     private float mouseX;
     private float mouseY;
     private float rotX = 0f;
@@ -15,28 +14,29 @@ public class PlayerCam : MonoBehaviour
 
     public float sensitivity;
     public Transform playerTr;
+
     void Start()
     {
         lastMousePos = Mouse.current.position.ReadValue();
     }
     void Update()
     {
-        FirstPersonLook();
+        MiniCamFollow();
     }
     
-    void FirstPersonLook()
+    void MiniCamFollow()
     {
-        transform.position = playerTr.position;
+        //카메라 회전
         Vector2 delta = Mouse.current.delta.ReadValue() * sensitivity;
 
         mouseX = delta.x;
         mouseY = delta.y;
 
-        rotX -= mouseY;
-        rotX = Mathf.Clamp(rotX, -90f, 90f);
+        rotY += mouseX;
 
-        transform.localRotation = Quaternion.Euler(rotX,0f,0f);
+        transform.localRotation = Quaternion.Euler(90f,rotY,0f);
 
-        playerTr.Rotate(Vector3.up * mouseX);
+        //카메라 플레이어 추적
+        transform.position = new Vector3(playerTr.position.x , 30f, playerTr.position.z);
     }
 }
