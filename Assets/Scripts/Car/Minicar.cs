7b160db40be26935cacc9f2b695b876fe7a97dc0
HobakDG <cil1216@naver.com>
2025-03-31 22:27:23 +0900

    [merge][fix][doc] merge RwR to develop

--- Assets/Scripts/Car/Minicar.cs
+++ Assets/Scripts/Car/Minicar.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController : MonoBehaviour
+public class Minicar : MonoBehaviour
 {
     public enum Axel { Front, Rear };
 
