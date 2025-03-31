50b4f9204eb943da3fc4ce94a659dd6e539975ef
fuzzyter <165622596+fuzzyter@users.noreply.github.com>
2025-03-31 21:08:02 +0900

    carMoveAndRotation
    차량 에셋 적용, 이동, 카메라 시점, 핸들 및 바퀴 회전

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public enum Axel
+    {
+        Front,
+        Rear
+    }
+
+    [Serializable]
+    public struct Wheel
+    {
+        public GameObject wheelModel;
+        public WheelCollider wheelCollider;
+        public Axel axel;
+    }
+
+    public float maxAcceleration = 30.0f;
+    public float brakeAcceleration = 50.0f;
+
+    public float turnSensitivity = 1.0f;
+    public float maxSteerAngle = 30.0f;
+
+    public Vector3 _centerOfMass;
+
+    public List<Wheel> wheels;
+    public GameObject steeringWheel; //핸들
+    private Quaternion initialSteeringRotation;
+
+    float moveInput;
+    float steerInput;
+
+    private Rigidbody carRb;
+
+    void Start()
+    {
+        carRb = GetComponent<Rigidbody>();
+        carRb.centerOfMass = _centerOfMass;
+
+        if (steeringWheel != null) //핸들
+        {
+            initialSteeringRotation = steeringWheel.transform.localRotation; // 초기 회전값 저장
+        }
+    }
+
+    void Update()
+    {
+        GetInputs();
+        AnimateWheels();
+    }
+
+    void LateUpdate()
+    {
+        Move();
+        Steer();
+        Brake();
+    }
+
+    void GetInputs()
+    {
+        moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
+    }
+
+    void Move()
+    {
+        foreach (var wheel in wheels) { wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration * Time.deltaTime; }
+    }
+    /*
+    void Steer()
+    {
+        foreach(var wheel in wheels)
+        {
+            if(wheel.axel == Axel.Front)
+            {
+                var _steerAngle = steerInput * turnSensitivity * maxSteerAngle;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+            }
+        }
+    }*/
+    void Steer()
+    {
+        float _steerAngle = steerInput * turnSensitivity * maxSteerAngle;
+
+        foreach (var wheel in wheels)
+        {
+            if (wheel.axel == Axel.Front)
+            {
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+            }
+        }
+
+        if (steeringWheel != null)
+        {
+            Quaternion targetRotation = initialSteeringRotation * Quaternion.Euler(0, _steerAngle * 2, 0);
+            steeringWheel.transform.localRotation = Quaternion.Lerp(steeringWheel.transform.localRotation, targetRotation, Time.deltaTime * 5f);
+        }
+    }
+
+    void Brake()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            foreach(var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = 300 * brakeAcceleration * Time.deltaTime;
+            }
+        }
+        else
+        {
+            foreach( var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = 0;
+            }
+        }
+    }
+
+    void AnimateWheels()
+    {
+        foreach(var wheel in wheels)
+        {
+            Quaternion rot;
+            Vector3 pos;
+            wheel.wheelCollider.GetWorldPose(out pos, out rot);
+            wheel.wheelModel.transform.position = pos;
+            wheel.wheelModel.transform.rotation = rot;
+        }
+    }
+}
