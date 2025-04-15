a2a16c56511efdabd4758b3eb52741b044483de4
ivorycoffee <jskim331933@naver.com>
2025-04-15 20:59:50 +0900

    [feat] BoomGate 구조물, 컷신에 나올 플레이어 타임라인 제작중,[etc] 폴더 구조 변경

---
+++ Assets/signal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class signal : MonoBehaviour
+{
+
+    public void ReceiveSignal()
+    {
+        Debug.Log("시그널");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
