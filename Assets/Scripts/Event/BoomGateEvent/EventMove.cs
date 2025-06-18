740d94549b91a7fa61111fdfd4c09c5c8c20648f
ivorycoffee <jskim331933@naver.com>
2025-06-18 11:35:28 +0900

    [feat][fix][docs]차단바 이벤트 navmesh로 변경, 퓨즈 꺼지는 소리, 걷는 소리, 차단바 올라가는 소리 적용

--- Assets/Scripts/Event/BoomGateEvent/EventMove.cs
+++ Assets/Scripts/Event/BoomGateEvent/EventMove.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.AI;
 using System.Collections;
 using System.Collections.Generic;
+using FMOD.Studio;
+using FMODUnity;
 
 public class EventMove : MonoBehaviour
 {   
+    private NavMeshAgent agent;
+    private EventInstance playerFootSteps;
+    private EventInstance boomGateBarSound;
+    private EventInstance carLight;
+    private EventInstance fuseOff;
     public Animator animator;
     public Transform target;       // BoomGate의 Transform
     public Transform boomgatebar;
@@ -28,20 +36,44 @@ public class EventMove : MonoBehaviour
         BoomGateEventTrigger.isBoomEvent = false;
         this.gameObject.SetActive(false);
         CarRb.isKinematic = false;
+        CarRb.velocity = Vector3.zero;
         //BoomGateEventTrigger off
    }
+   void Awake()
+    {  
+        agent = GetComponent<NavMeshAgent>();
+        playerFootSteps = AudioManager.instance.CreateInstance(FMODEvents.instance.playerFootSteps);
+        boomGateBarSound = AudioManager.instance.CreateInstance(FMODEvents.instance.boomGateBarSound);
+        carLight = AudioManager.instance.CreateInstance(FMODEvents.instance.carLight);
+        fuseOff = AudioManager.instance.CreateInstance(FMODEvents.instance.fuseOff);
+    }
+
     private IEnumerator MoveRoutine()
-    {
-        animator = GetComponent<Animator>();
+    {   
         Vector3 destination = target.position;
         Vector3 startPos = transform.position;
 
+        animator = GetComponent<Animator>();
+        agent.enabled = true;
+        agent.SetDestination(destination);
+        Vector3 dir = (destination - transform.position).normalized;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 5f);
+        
 
         // 1. 이동 및 헤드라이트 off
         animator.SetInteger("Movement", 0);
-        yield return StartCoroutine(BGEvent_Lightoff()); //이벤트 진입 시 헤드라이트 off
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(playerFootSteps, transform, GetComponent<Rigidbody>());
+        playerFootSteps.start();
+        agent.stoppingDistance = 0.5f;
+        while (agent.pathPending || agent.remainingDistance > agent.stoppingDistance)
+        {
+            yield return null;
+        }
+        agent.isStopped = true;
+        yield return StartCoroutine(BGEvent_Lighton()); //이벤트 진입 시 헤드라이트 on
         // yield return StartCoroutine(BGEvent_Lighton());
-        yield return StartCoroutine(BGEvent_Move(destination)); // 차단바까지 이동
+        // yield return StartCoroutine(BGEvent_Move(destination)); // 차단바까지 이동
+        playerFootSteps.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         animator.SetInteger("Movement", 1); // 차단바 주섬주섬 애니메이션
         yield return new WaitForSecondsRealtime(3f); // 3초 기다림
 
@@ -57,6 +89,7 @@ public class EventMove : MonoBehaviour
         yield return StartCoroutine(BGEvent_Rotate());
         yield return StartCoroutine(BGEvent_WaveInactive());
         yield return StartCoroutine(BGEvent_Lightoff());
+        AudioManager.instance.PlayOneShot(FMODEvents.instance.fuseOff, this.transform.position);
         yield return new WaitForSecondsRealtime(3.5f);
         // 여기 퓨즈나가는 소리 추가
         yield return StartCoroutine(BGEvent_Rotate());
@@ -68,9 +101,13 @@ public class EventMove : MonoBehaviour
         animator.SetInteger("Movement", 4);
         yield return StartCoroutine(BGEvent_Rotate());
         animator.SetInteger("Movement", 5);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(playerFootSteps, transform, GetComponent<Rigidbody>());
+        playerFootSteps.start();
         yield return StartCoroutine(BGEvent_Return(startPos));
         yield return new WaitForSecondsRealtime(1.0f);
-    }
+        playerFootSteps.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        playerFootSteps.release();
+    }   
 
     private IEnumerator BGEvent_Move(Vector3 destination){
         while (Vector3.Distance(transform.position, destination) > 0.45f)
@@ -96,16 +133,26 @@ public class EventMove : MonoBehaviour
         transform.rotation = endRot;
         }
     private IEnumerator BGEvent_Return(Vector3 startPos){
-        while (Vector3.Distance(transform.position, startPos) > 0.1f)
+        agent.isStopped = false;
+        agent.SetDestination(startPos);
+
+        while (agent.pathPending || agent.remainingDistance > agent.stoppingDistance)
         {
-        transform.position = Vector3.MoveTowards(transform.position, startPos, speed * Time.deltaTime);
-        yield return null;
+            yield return null;
         }
+
+        agent.isStopped = true;
+        // while (Vector3.Distance(transform.position, startPos) > 0.1f)
+        // {
+        // transform.position = Vector3.MoveTowards(transform.position, startPos, speed * Time.deltaTime);
+        // yield return null;
+        // }
         }
 
     private IEnumerator BGEvent_Lighton()
     {   
         light.SetActive(true);
+        AudioManager.instance.PlayOneShot(FMODEvents.instance.carLight, this.transform.position);
         yield return null;
     }
     
@@ -127,13 +174,15 @@ public class EventMove : MonoBehaviour
         light.SetActive(true);
         yield return new WaitForSecondsRealtime(0.1f);
         }
+
+
         //num 만큼 반복
 
     }
 
     private IEnumerator BGEvent_BoomGateOpen()
     {   
-       
+        AudioManager.instance.PlayOneShot(FMODEvents.instance.boomGateBarSound, target.transform.position);
         Quaternion startRot = boomgatebar.transform.rotation;
         Quaternion endRot = startRot * Quaternion.Euler(0, -90, 0);
         float elapsed = 0f;
